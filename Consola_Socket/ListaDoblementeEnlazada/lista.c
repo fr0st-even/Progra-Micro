205b49f5e6cab75815cda3dabc5e47bc62120496
fr0st-even <iosef.joelcv@gmail.com>
2026-04-29 11:25:24 -0600

    Carga de archivo lista.c
    Carga del archivo lista.c la cual fue actualizada y modificada para que funcione con puntero siguiente y puntero anterior, se agrego la funcion de recorre la lista del ultimo al primer elemento

---
+++ Consola_Socket/ListaDoblementeEnlazada/lista.c
@@ -0,0 +1,144 @@
+#include "lista.h"
+
+int estaVacia( Nodo *ini ){ return NULL == ini; }
+
+Nodo *crearMemoria( void *dato, Nodo *ptr_sig, Nodo *ptr_ant ) {
+    Nodo *tmp = (Nodo*) malloc( sizeof(Nodo) );
+    if( tmp != NULL ) {
+        tmp -> dato = dato;
+        tmp -> ptr_sig = ptr_sig;
+        tmp -> ptr_ant = ptr_ant;
+    }
+    return tmp;
+}
+
+void insertarAlFrente( Nodo **ini, Nodo **fin, void *inf ) {
+    if( estaVacia( *ini ) == 1 )
+        *ini = *fin = crearMemoria( inf, NULL, NULL );
+    else {
+        Nodo *nuevo = crearMemoria( inf, *ini, NULL );
+        (*ini) -> ptr_ant = nuevo;
+        *ini = nuevo;
+    }
+}
+
+void insertarAlFinal( Nodo **ini, Nodo **fin, void *inf ) {
+    if( estaVacia( *ini ) == 1 )
+        *ini = *fin = crearMemoria( inf, NULL, NULL );
+    else {
+        Nodo *nuevo = crearMemoria( inf, NULL, *fin );
+        (*fin) -> ptr_sig = nuevo;
+        *fin = nuevo;
+    }
+}
+
+void *removerAlFrente( Nodo **ini, Nodo **fin, int *errno ) {
+    if( 1 == estaVacia(*ini) ){
+        (*errno) = 1;
+        return NULL;
+    }
+    
+    void *dato = (*ini) -> dato;
+    
+    if(*ini==*fin)
+        *ini = *fin = NULL;
+    else {
+        *ini = (*ini) -> ptr_sig;
+        (*ini) -> ptr_ant = NULL;
+    }
+    
+    (*errno) = 0;
+    return dato;
+}
+
+void imprimirLista( Nodo *ini, void (*func)( void *inf ) ) {
+    Nodo *actual = ini;
+
+    if( NULL == ini ) {
+        printf( "La lista esta vacia\n" );
+        return;
+    }
+    
+    while( actual != NULL ) {
+        func( actual -> dato );
+        actual = actual -> ptr_sig;
+    }
+
+    printf( "\n" );
+}
+
+void imprimirListaReversa( Nodo *fin, void (*func)( void *inf ) ) {
+    Nodo *actual = fin;
+
+    if( NULL == fin ) {
+        printf( "La lista esta vacia\n" );
+        return;
+    }
+    
+    while( actual != NULL ) {
+        func( actual -> dato );
+        actual = actual -> ptr_ant;
+    }
+
+    printf( "\n" );
+}
+
+int contarElementosLista( Nodo *ini ) {
+    int contador = 0;
+    Nodo *actual = ini;
+
+    if( NULL == ini ) {
+        contador = -1;
+        return contador;
+    }
+    
+    while( actual != NULL ) {
+        contador++;
+        actual = actual -> ptr_sig;
+    }
+
+    return contador;
+}
+
+int buscarDentroDeLista( Nodo *ini
+        , int (*func)( void *inf, void *clave )
+        , void *clave, void **i ) {
+
+    Nodo *actual = ini;
+    if( NULL == ini ) {
+        //printf( "La lista esta vacia\n" );
+        return -1;
+    }
+    
+    while( actual != NULL ) {
+        if( func( actual -> dato, clave ) == 1 ) {
+            *i = actual -> dato;
+            return 1;
+        }
+        actual = actual -> ptr_sig;
+    }
+
+    return 0;
+}
+
+void buscarDentroDeLista( Nodo *ini
+        , int (*func)( void *inf, void *clave )
+        , void *clave, Nodo **iniB, Nodo **finB ) {
+
+    Nodo *actual = ini;
+    if( NULL == ini ) {
+        //printf( "La lista esta vacia\n" );
+    }
+    
+    while( actual != NULL ) {
+        if( func( actual -> dato, clave ) == 1 )
+            insertarAlFinal( iniB, finB, (actual -> dato) );
+        
+        actual = actual -> ptr_sig;
+    }
+}
+
+//void liberarInfo( void *dato ) {
+//    Info *inf = (Info*) dato;
+//    free( inf );
+//}
